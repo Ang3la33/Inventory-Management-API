d1a0ff15fbd98d311ee21ad375e15e6124486942
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-03-08 23:43:40 -0330

    Added CloudWatch Policy to Lambda Function

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -41,3 +41,30 @@ resource "aws_iam_role_policy_attachment" "lambda_dynamodb_attachment" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.dynamodb_access_policy.arn
 }
+
+# IAM Policy for Lambda to write logs to CloudWatch
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "LambdaLoggingPolicy"
+  description = "Allows Lambda to write logs to CloudWatch"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
+# Attach CloudWatch Policy
+resource "aws_iam_role_policy_attachment" "lambda_logging_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
