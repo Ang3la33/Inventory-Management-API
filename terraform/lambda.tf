76632968604d89ef3131fda0d0ca129922626770
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-03-08 22:29:50 -0330

    Updated lambda.tf to ensure the Lambda function uses the new IAM role

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,11 +3,11 @@ resource "aws_lambda_function" "inventory_lambda" {
     role             = aws_iam_role.lambda_role.arn
     runtime          = "python3.12"
     handler          = "lambda_function.lambda_handler"
-    filename         = "${path.module}/lambda_function.zip"   # Ensure correct path
+    filename         = "${path.module}/lambda_function.zip"   
 
     environment {
         variables = {
-            TABLE_NAME = "InventoryTable"
+            TABLE_NAME = aws_dynamodb_table.inventory_table.name
         }
     }
 }
