30a5625baca54282136d7ada7876011932a31ceb
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-03-08 22:37:18 -0330

    Removed IAM role from main as its in a seperate file for cleaner code practices

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,21 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda_execution_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
-
 resource "aws_iam_policy_attachment" "lambda_basic_execution" {
   name       = "lambda_basic_execution"
   roles      = [aws_iam_role.lambda_role.name]
