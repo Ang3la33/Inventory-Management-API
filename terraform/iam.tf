6a5193bc631fca49e52cd000bdacbb39be86b061
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-03-08 22:27:20 -0330

    Added iam terraform file that creates an IAM role for lambda function, an IAM policy for lambda to access dynamodb, and then attached the policy to the Lambda IAM Role

---
+++ terraform/iam.tf
@@ -0,0 +1,42 @@
+# IAM Role for Lambda Function 
+resource "aws_iam_role" "lambda_role"
+name = "lambda_execution_role"
+
+assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+            Service = "lambda.amazonaws.com"
+        }
+    }]
+})
+
+# IAM Policy for Lambda to Access DynamoDB 
+resource "aws_iam_policy" "dynamodb_access_policy" {
+    name        = "DynamoDBAccessPolicy"
+    description = "Allows Lambda to read and write to InventoryTable"
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+            Effect = "Allow"
+            Action = [
+                "dynamodb:GetItem",
+                "dynamodb:PutItem",
+                "dynamodb:UpdateItem",
+                "dynamodb:DeleteItem",
+                "dynamodb:Scan",
+                "dynamodb:Query"
+            ]
+            Resource = aws_dynamodb_table.inventory_table.arn
+        }]
+    })
+}
+
+# Attach Policy to Lambda IAM Role 
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_attachment" {
+    role       = aws_iam_role.lambda_role.name
+    policy_arn = aws_iam_policy.dynamodb_access_policy.arn
+}
